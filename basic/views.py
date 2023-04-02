6359a35e66eda30ad99459892edf99b68322a9b8
vinay sharma <vinaysharma5335@gmail.com>
2023-04-02 14:13:39 +0530

    change in response

--- basic/views.py
+++ basic/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Pooja")
+    return HttpResponse("Vinay ❤️ Pooja")
 
 # Create your views here.
